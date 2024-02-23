adadeaa20a55403566702890fefc659c09f56cdb
CodeYourFuture <codeyourfuture@CodeYourFutures-MacBook-Pro.local>
2024-02-23 17:55:56 +0000

    add aws security group

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+
+# Create a new S3 bucket policy
 resource "aws_s3_bucket" "new_bucket" {
   bucket = "dev-planner"
 
@@ -6,7 +8,6 @@ resource "aws_s3_bucket" "new_bucket" {
     Environment = "Dev"
   }
 }
-
 
 resource "aws_s3_bucket_policy" "dev-planner_policy" {
   bucket = aws_s3_bucket.new_bucket.id
@@ -26,3 +27,28 @@ resource "aws_s3_bucket_policy" "dev-planner_policy" {
 }
 POLICY
 }
+
+# Create a new security group
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2-security-group"
+  description = "New security group for EC2 instances"
+
+  vpc_id = "vpc-0f511b17db48eda52"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+}
+
+
+
